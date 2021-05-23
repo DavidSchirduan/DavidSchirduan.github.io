36e5a6acc51cdfe0de443781c58b06a80bc0d901
David Schirduan <davids@ceterus.com>
2021-05-23 00:13:17 -0400

    functional test

--- assets/generator_resources/test.js
+++ assets/generator_resources/test.js
@@ -11,6 +11,7 @@ fetch('/assets/generator_resources/test.json')
       // Examine the text in the response
       response.json().then(function(data) {
         wyrd = data;
+        grabParamsURL();
       });
     }
   )
@@ -18,36 +19,69 @@ fetch('/assets/generator_resources/test.json')
     console.log('Fetch Error :-S', err);
   });
 
-function beginHunt() {
+function grabParamsURL(){
+  //if someone is loading a character code
+  if (window.location.search != ""){
+    console.log("Seed:" + window.location.search);
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('woods') && urlParams.get('target')){
+      //populate the generator with the saved info
+      beginHunt(decodeURI(urlParams.get('woods')), decodeURI(urlParams.get('target')));
+    } else {
+      console.log("invalid code, using new code");
+    }
+  } else {
+    console.log("no params, using new code");
+  }
+}
 
-  var target = wyrd.creatures[Math.floor(Math.random() * wyrd.creatures.length)];
+var locationList; //contains all the html and text for each location
 
-  document.getElementById("huntText").innerHTML = "Within the <strong>" + wyrd.names.prefix[Math.floor(Math.random() * wyrd.names.prefix.length)] + " Of " + wyrd.names.suffix[Math.floor(Math.random() * wyrd.names.suffix.length)] + "</strong> there is rumored to be " + wyrd.secrets[Math.floor(Math.random() * wyrd.secrets.length)] + " But beware! " + wyrd.dangers[Math.floor(Math.random() * wyrd.dangers.length)] + "<br><br> You are hunting one or more <strong>" + target.name + "</strong> (pg. " + target.page + ") for fortune, fame, flesh, or some other reason altogether. You will need <strong>" + target.marks + " Marks</strong> to track the creature(s).";
+function newHunt(){
+  woods = wyrd.names.prefix[Math.floor(Math.random() * wyrd.names.prefix.length)] + " of " + wyrd.names.suffix[Math.floor(Math.random() * wyrd.names.suffix.length)];
 
-  //pick the paths we'll be using
-  var allPaths = ["A","B","C","D","E","F","G","H"];
-  pathNotes = [];
+  target = wyrd.creatures[Math.floor(Math.random() * wyrd.creatures.length)].name;
+
+  beginHunt(woods, target);
+}
+
+function beginHunt(seedWoods, seedTarget) {
+  myrng = new Math.seedrandom(seedWoods + seedTarget);
+
+  locationList = [];
+  document.getElementById("locationText").innerHTML = "";
+
+  document.getElementById("huntText").innerHTML = "Within the <strong>" + seedWoods + "</strong> there is rumored to be " + wyrd.secrets[Math.floor(myrng() * wyrd.secrets.length)] + " But beware! " + wyrd.dangers[Math.floor(myrng() * wyrd.dangers.length)] + "<br><br> You are hunting one or more <strong>" + seedTarget + "</strong> for fortune, fame, flesh, or some other reason altogether. You will need <strong>" + (Math.floor(myrng() * 20) + 1) + " Marks</strong> to track the creature(s).";
+
+  //MAP GENERATION
+  allPaths = ["A","B","C","D","E","F","G","H"];
   pathList = [];
+  pathNotes = [];
+  //1. pick a random path (A-H)
+  pathList.push(allPaths.splice(Math.floor(myrng() *allPaths.length), 1));
+  //2. give that path a random description
+  pathNotes.push(wyrd.pathDescriptions[Math.floor(myrng() * wyrd.pathDescriptions.length)]);
+  //3. Pick another random Path
+  pathList.push(allPaths.splice(Math.floor(myrng() *allPaths.length), 1));
+  //2. give that path a random description
+  pathNotes.push(wyrd.pathDescriptions[Math.floor(myrng() * wyrd.pathDescriptions.length)]);
 
-  pathList.push(allPaths.splice(Math.floor(Math.random()*allPaths.length), 1));
-  pathNotes.push(wyrd.pathDescriptions[Math.floor(Math.random() * wyrd.pathDescriptions.length)]);
-  pathList.push(allPaths.splice(Math.floor(Math.random()*allPaths.length), 1));
-  pathNotes.push(wyrd.pathDescriptions[Math.floor(Math.random() * wyrd.pathDescriptions.length)]);
+  //now we have a random list of letters that we attach paths to.
+  //pathNotes is the same size as pathList, so we use the same index when referencing it
 
+  //Pull images of the matching two paths and layer them
   document.getElementById("mapIMG").innerHTML = "<img src=\"/images/WyrdMaps/blankMap.png\">";
   document.getElementById("mapIMG").style = "background-image: url(/images/WyrdMaps/path"+pathList[0]+".png),url(/images/WyrdMaps/path"+pathList[1]+".png);background-size: contain;background-position: center;background-repeat: no-repeat;";
 
-  //now we have a random list of letters that we attach paths to.
-  //pathNotes is the same size as pathList, so we use the same index when referencing it.
+  //LOCATION GENERATION (8)
 
-  //populate the map locations with 8 things
-  for (i = 1; i < 9; i++){
-    var nextLocation = wyrd.locations[Math.floor(Math.random() * wyrd.locations.length)];
-    var locationtext = "<div class=\"h3\">" + i + ". ";
-    locationtext = locationtext + nextLocation.name + "</div>";
-    locationtext = locationtext + "<p>" + nextLocation.description + "</p>";
+  logHTML = "<div class=\"logItem\"><a onclick=\"wy_nextEncounter()\"><p><span class=\"logWyrdLevel\" style=\"color:red;\">!</span>Random Encounter</p></a></div>";
 
-    document.getElementById("locbutton"+i).innerHTML = "<a href=\"#loc"+i+"\"><p><span class=\"logWyrdLevel\">"+i+"</span>"+nextLocation.name+"</p></a>";
+  for (i = 0; i < 8; i++){
+    nextLocation = wyrd.locations[Math.floor(myrng() * wyrd.locations.length)];
+  
+    //Build out the buttons
+    logHTML = logHTML + "<div class=\"logItem\"><a onclick=\"wyrd_getLoc('"+i+"')\"><p><span class=\"logWyrdLevel\">" + (i+1) + "</span> " + nextLocation.name + "</p></a></div>";
 
     //now see if any of our chosen paths have exits in this location
     directionKeys = {
@@ -61,23 +95,38 @@ function beginHunt() {
       "WW" : "West"
     }
 
-    exitText = "<ul>";
+  exitText = "<br><ul>";
 
-    //for each path on our list, construct the exits of each location
-    for (path = 0; path < pathList.length; path++){
-      //get the exits array for path X
-      exits = wyrd.paths[pathList[path]];
-      for (var exit of exits){
-        if (exit[0] == i){ //if the current location has an exit in any of our paths
-          direction = exit.substring(1);
-          exitText = exitText + "<li><strong>" + directionKeys[direction] + "</strong>: " + pathNotes[path] + " " + wyrd.scenes[Math.floor(Math.random() * wyrd.scenes.length)] + " " + wyrd.woods[Math.floor(Math.random() * wyrd.woods.length)] +"<br>"+ wyrd.senses[Math.floor(Math.random() * wyrd.senses.length)] + "</li>";
-        } 
+  //for each path on our list, construct the exits of each location
+  for (path = 0; path < pathList.length; path++){
+    //get the exits array for path X
+    exits = wyrd.paths[pathList[path]];
+    for (var exit of exits){
+      if (exit[0] == i+1){ //if the current location has an exit in any of our paths
+        direction = exit.substring(1);
+        exitText = exitText + "<li><strong>" + directionKeys[direction] + "</strong>: " + pathNotes[path] + " " + wyrd.scenes[Math.floor(myrng() * wyrd.scenes.length)] + " " + wyrd.woods[Math.floor(myrng() * wyrd.woods.length)] +"<br>"+ wyrd.senses[Math.floor(myrng() * wyrd.senses.length)] + "</li>";
       }
     }
+  }
 
-    locationtext = locationtext + exitText;
-    document.getElementById("loc" + i).innerHTML = locationtext;
-    }
+  locationtext = nextLocation.description + exitText;
+  locationList.push("<h3>" + nextLocation.name + "</h3><p>" + locationtext + "</p>");
+  }
+  //make the buttons
+  document.getElementById("logContent").innerHTML = logHTML;
+
+  //set url
+  document.title = seedWoods; 
+  window.history.replaceState(null, null, "?woods="+seedWoods+"&target="+seedTarget);
+  document.getElementById("saveCharacter").innerHTML = "<i>Bookmark this page to save this hunt,<br>or <a href=\"" + window.location.href + "\"> copy this link</a>.</i>";
+  
+}
+
+//display the location they click on
+function wyrd_getLoc(x){
+
+  document.getElementById("locationText").innerHTML = locationList[x];
+  
 }
 
 function wy_nextEncounter() {
@@ -85,19 +134,19 @@ function wy_nextEncounter() {
     var encounterText = "";
 
     switch (true) {
-      case (percentage <= 20):
+      case (percentage <= 33):
         var plant = wyrd.plants[Math.floor(Math.random() * wyrd.plants.length)];
         encounterText = encounterText + "<h3 class=\"tightSpacing\">Plant: " + plant.name + "</h3>" + plant.description +
           "<br><strong>Uses:</strong> " + plant.uses;
         break;
-      case (percentage > 20 && percentage <= 40):
+      case (percentage > 33 && percentage <= 66):
         var trap = wyrd.traps[Math.floor(Math.random() * wyrd.traps.length)];
         encounterText = encounterText + "<h3 class=\"tightSpacing\">Trap: " + trap.name + "</h3>" + trap.description +
           "<br><strong>Detect:</strong> " + trap.detect + 
           "<br><strong>Effect:</strong> " + trap.effect + 
           "<br><strong>Disable/Avoid:</strong> " + trap.disable;          
         break;
-      case (percentage > 40 && percentage <= 80):
+      case (percentage > 66):
         var creature = wyrd.creatures[Math.floor(Math.random() * wyrd.creatures.length)];
         encounterText = encounterText +  "<h3 class=\"tightSpacing\">Creature: " + creature.name + " <i>(pg. " + creature.page + ")</i></h3>" +
           "<strong>Quantity:</strong> " + creature.quantity +
@@ -112,7 +161,7 @@ function wy_nextEncounter() {
       default:
         encounterText = encounterText +  "No Encounter. Just an empty, restless silence.";
     }
-document.getElementById("encounterCard").innerHTML = encounterText;
-document.getElementById("encounterCard").scrollIntoView();
   
+  document.getElementById("locationText").innerHTML = encounterText;  
+
 }
--- hunttest.md
+++ hunttest.md
@@ -8,51 +8,28 @@ published: true
 
 <div class="stygian-card">
   <div class="stygian-text" id="stygian-complete">
-    <div id="encounterContent"></div>
-    <p id="huntText"></p>
-    <div id="mapIMG"></div>
-    <div id="encounterCard"></div>    
-    <div id="levelContent">
-    <p id="loc1"></p>
-    <p id="loc2"></p>
-    <p id="loc3"></p>
-    <p id="loc4"></p>
-    <p id="loc5"></p>
-    <p id="loc6"></p>
-    <p id="loc7"></p>
-    <p id="loc8"></p>
+    <div class="row">
+      <div class="col-12 col-md-4" style="text-align:center;">
+        <button class="stygian-button" type="button" onclick="newHunt()">Begin New Hunt</button>
+        <p id="huntText" style="text-align:left;"></p>
+        <p id="saveCharacter" style="text-align:center;"></p>
+      </div>
+      <div class="col-12 col-md-8">
+        <div id="mapIMG"></div>
+      </div>
     </div>
-  </div>
-  <div class="stygian-log">
-    <div class="stygian-buttons">
-      <button id="deeperButton" class="stygian-button" type="button" onclick="beginHunt()">Begin Hunt</button>
-      <button class="stygian-button" type="button" onclick="wy_nextEncounter()">Random Encounter</button>
-      <button class="stygian-button" type="button">Random Discovery</button>
-    </div>
-    <hr class="stygian-hr-dark">
-    <div>
-      <h3 style="margin-top: -20px;">
-        Locations
-      </h3>
-      <div id="logContent">
-      <div class="logItem"><a href="#mapIMG"><p><span class="logWyrdLevel">M</span>View Map</p></a></div>
-      <div class="logItem" id="locbutton1"></div>
-      <div class="logItem" id="locbutton2"></div>
-      <div class="logItem" id="locbutton3"></div>
-      <div class="logItem" id="locbutton4"></div>
-      <div class="logItem" id="locbutton5"></div>
-      <div class="logItem" id="locbutton6"></div>
-      <div class="logItem" id="locbutton7"></div>
-      <div class="logItem" id="locbutton8"></div>
-      <div class="logItem"><a href="#mapIMG"><p><span class="logWyrdLevel">M</span>View Map</p></a></div>
+    <div id="encounterCard"></div>
+    <hr class="stygian-hr">
+    <div class="row">
+      <div class="col-12 col-md-4">
+        <div id="logContent">
+        </div>
+      </div>
+      <div id="locationText" class="col-12 col-md-8">
       </div>
     </div>
   </div>
 </div>
-
-
-
-
 
 <!--Necessary for allowing the sticky buttons and background changes-->
 <style>
@@ -68,19 +45,7 @@ published: true
   margin-top: 0px;
   }
 
- /* Necessary for the pdf download*/
-
-  .stygian-text ul {
-  list-style: none; /* Remove default bullets */
-}
-
-.stygian-text ul li::before {
-  content: ">";  /* Add content: \2022 is the CSS Code/unicode for a bullet */
-  font-weight: bold; /* If you want it to be bold */
-  display: inline-block; /* Needed to add space between the bullet and the text */
-  width: 1em; /* Also needed for space (tweak if needed) */
-  margin-left: -1em; /* Also needed for space (tweak if needed) */
-}
 </style>
 
+<script async src="/assets/js/seedrandom.min.js" language="javascript" type="text/javascript"></script>
 <script async src="/assets/generator_resources/test.js" language="javascript" type="text/javascript"></script>
