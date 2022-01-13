c56d3672bfab9e75016b6c185cf028fa7c319581
David Schirduan <davidschirduan@gmail.com>
2022-01-13 16:49:01 -0500

    Update troika.js

--- assets/generator_resources/troika.js
+++ assets/generator_resources/troika.js
@@ -396,10 +396,10 @@ function tr_flipCard(token) {
       //flip just flip 180
       tr_degrees = tr_degrees + 180;
       bgImage = "url('/images/troika_end_of_round.png')";
-      cardTxt = "End of Round";
+      cardTxt = "End of <br>Round";
       bgColor = "white";
       turnText = "<p style=\"margin: unset;\">" + turnNumber + ". End Round" + "</p>" + turnText;
-      document.getElementById("delayButton").innerText = "Keep Going";
+      //document.getElementById("delayButton").innerText = "Keep Going";
       break;
 
     default:
