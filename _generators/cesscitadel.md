99ad2c2c70478e67c7f5b1530cd1821638664fb8
David Schirduan <davidschirduan@gmail.com>
2022-10-23 23:14:45 -0400

    finished first pass

--- _generators/cesscitadel.md
+++ _generators/cesscitadel.md
@@ -1,30 +1,46 @@
 ---
-date: 2022-10-08
+date: 2022-10-23
 layout: full-page
-title: City Intersection Generator
+title: Cess & Citadel Intersection Generator
 permalink: cess-intersection
-published: false
+published: true
 hide_description: true
-image: /images/posts/wyrd_generator.png
+image: /images/CessMaps/cover.png
 description: >
-  A mobile-friendly Hunt generator for the Cess and Citadel.
+  A mobile-friendly city intersection generator for the Cess & Citadel.
 ---
 
 <div class="stygian-card">
   <div class="stygian-text">
     <div class="row">
-      <div class="col-12 col-md-5" style="text-align:center;">
-        <div class="logItem"><a onclick="newHunt()"><h3>Begin New Hunt</h3></a></div>
-        <p id="huntText" style="text-align:left;">Simply click the button above to generate a Wyrd Hunt. It includes a map, 6 locations, random encounters, and more; all at the click of a button. If you like what you see, you'll love the full book even more!</p>
-        <p id="saveHunt" style="text-align:center;"></p>
-        <div id="mapIMG"></div>
-      </div>
-      <div class="col-12 col-md-7">
-        <div id="logContent" class="row" style="justify-content:center;margin-bottom:50px;">
-        <h3><i>The woods do not care for you. Never forget that.</i></h3>
-        <p>Into the Wyrd and Wild is a supplemental book for those seeking to incorporate a weird and terrifying wilderness into their role-playing game. Players and GMs who enjoy a level of horror and prefer the sweeping, darkened landscapes of forest and mires to the well-trodden cobblestone of dungeons need look no further when it comes to books. Presented within the book is a light overhaul of the adventuring system, modified to fit better with a campaign centered around forays into the frightening wilderness.</p>
+      <div class="col-12 col-md-4">
+        <div id="logContent" class="row" style="justify-content:space-evenly;margin-bottom:30px;">
+          <div class="logItem col-12 col-md-6"><a onclick="generateNPC()"><h3>NPC</h3></a></div>
+          <div class="logItem col-12 col-md-6"><a onclick="randEncounter()"><h3>Encounter</h3></a></div>
         </div>
-        <div id="locationText"></div>
+        <div id="randBtn"></div>
+        <div id="mapIMG"></div>
+        <div class="logItem"><a onclick="generateIntersection()"><h3>Generate Another Intersection</h3></a></div>
+        <p><a href="https://www.thiscityeatspeople.com/">Into the Cess and Citadel</a> is a supplemental TTRPG book for those seeking to incorporate a strange, colorful, and terrifying city into their role-playing game. Presented within the book is a comprehensive guide to running adventures or campaigns within a fantastical city, along with an overhaul of the adventuring system to better accommodate the unique challenges and benefits of a sprawling urban environment. <a href="https://www.thiscityeatspeople.com/">Buy it here!</a></p>
+      </div>
+      <div class="col-12 col-md-8">
+        <h2 style="text-align: center;">Intersection of ...</h2>
+        <div id="logContent" class="row" style="justify-content:center;margin-bottom:30px;">
+          <div id="street1" class="cessBlock col-12 col-md-5"></div>
+          <div class="cessBlock col-1"><h2>&</h2></div>
+          <div id="street2" class="cessBlock col-12 col-md-5"></div>
+        </div>
+        <h2 style="text-align: center;">City Block Locations</h2>
+        <p id="blockDetails"></p>
+        <div id="logContent" class="row" style="justify-content:space-evenly;margin-bottom:30px;">
+          <div id="blueBlock" class="cessBlock col-12 col-md-5"></div>
+          <div id="greenBlock" class="cessBlock col-12 col-md-5"></div>
+        </div>
+        <div id="logContent" class="row" style="justify-content:space-evenly;margin-bottom:30px;">
+          <div id="redBlock" class="cessBlock col-12 col-md-5"></div>
+          <div id="yellowBlock" class="cessBlock col-12 col-md-5"></div>
+        </div>
+        <h2 style="text-align: center;"><i>This city eats people. Never forget that.</i></h2>
       </div>
     </div>
   </div>
--- assets/generator_resources/cess.js
+++ assets/generator_resources/cess.js
@@ -1,7 +1,7 @@
 //get the json file and parse it
 fetch('/assets/generator_resources/cess.json')
   .then(
-    function(response) {
+    function (response) {
       if (response.status !== 200) {
         console.log('Looks like there was a problem. Status Code: ' +
           response.status);
@@ -9,13 +9,13 @@ fetch('/assets/generator_resources/cess.json')
       }
 
       // Examine the text in the response
-      response.json().then(function(data) {
+      response.json().then(function (data) {
         cess = data;
-        grabParamsURL();
+        generateIntersection();
       });
     }
   )
-  .catch(function(err) {
+  .catch(function (err) {
     console.log('Fetch Error :-S', err);
   });
 
@@ -28,166 +28,243 @@ function selectRandom(jsonList) {
   return result;
 }
 
-function buildIntersection(){
+function generateIntersection() {
+
+  intersectionJSON = buildIntersection();
+
+  //Streets
+  document.getElementById("street1").innerHTML = "<h3><span style=\"color:purple;\">" + intersectionJSON.Street1.Name + "</span></h3><p>" + intersectionJSON.Street1.Appearance + "</p>";
+  document.getElementById("street2").innerHTML = "<h3><span style=\"color:orange;\">" + intersectionJSON.Street2.Name + "</span></h3><p>" + intersectionJSON.Street2.Appearance + "</p>";
+
+  //Set the map
+  document.getElementById("mapIMG").innerHTML = "<img src=\"/images/CessMaps/" + (Math.floor(Math.random() * 6) + 1) + ".png\">";
+
+  //describe block
+  document.getElementById("blockDetails").innerText = "This block is currently suffering from " + intersectionJSON.Issue[0] + ". " + intersectionJSON.Issue[1];
+
+  //Blue Shop
+  document.getElementById("blueBlock").innerHTML = intersectionJSON.Shop1;
+
+  //Green Shop
+  document.getElementById("greenBlock").innerHTML = intersectionJSON.Shop2;
+
+  //Red Block
+  document.getElementById("redBlock").innerHTML =
+    "<h3>" + intersectionJSON.Location1[0] + "</h3>" +
+    "<p>" + intersectionJSON.Location1[1] + "</p>";
+  //Yellow Block
+  document.getElementById("yellowBlock").innerHTML =
+    "<h3>" + intersectionJSON.Location2[0] + "</h3>" +
+    "<p>" + intersectionJSON.Location2[1] + "</p>";
+}
+
+function generateNPC() {
+  var npc;
+  if (Math.random() < .5) { //pick between two districts
+    npc = createNPC(district1);
+  } else {
+    npc = createNPC(district2);
+  }
+
+  npcHTML = "<h3>" + npc.Name + "</h3><p>A talented " + npc.Job + ". ";
+
+  if (npc.Appearance != "") {
+    npcHTML = npcHTML + "They look " + npc.Appearance + ". ";
+  }
+
+  if (npc.Mannerism != "") {
+    npcHTML = npcHTML + "They are " + npc.Mannerism + ". ";
+  }
+
+  npcHTML = npcHTML + npc.Quirk;
+
+  if (npc.Faction != "") {
+    npcHTML = npcHTML + "They are part of " + npc.Faction + ". ";
+  }
+
+  npcHTML = npcHTML + "</p><p>";
+
+  if (npc.Mod != "") {
+    npcHTML = npcHTML + "<p>" + npc.Mod + "</p>";
+  }
+
+  if (npc.Spell != "") {
+    npcHTML = npcHTML + "<p>They know the following spell, and may be willing to teach it: " + npc.Spell + "</p>";
+  }
+
+  if (npc.Items.length > 0) {
+    itemText = "<p><strong>Inventory:</strong><ul>";
+    for (item in npc.Items) {
+      itemText = itemText + "<li>" + npc.Items[item] + "</li>";
+    }
+    itemText = itemText + "</ul></p>";
+    npcHTML = npcHTML + itemText;
+  }
+
+  document.getElementById("randBtn").innerHTML = npcHTML;
+}
+
+function buildIntersection() {
   district1 = selectRandom(allDistricts);
   district2 = selectRandom(allDistricts);
-  
+
+  console.log("Dis1: " + district1 + " Dis2: " + district2);
+
   let Intersectionobj = {
-    issue: selectRandom(cess[district1].Issues.concat(cess[district2].Issues)),
-    feature: selectRandom(cess[district1].Features.concat(cess[district2].Features)),
-    street1: paveStreet(district1),
-    street2: paveStreet(district2),
-    shop1: createShop(district1),
-    shop2: createShop(district2),
-    location1: selectRandom(cess.General.Locations),
-    location2: selectRandom(cess.General.Locations)
+    "Issue": selectRandom(cess[district1].Issues.concat(cess[district2].Issues)),
+    "Feature": selectRandom(cess[district1].Features.concat(cess[district2].Features)),
+    "Street1": paveStreet(district1),
+    "Street2": paveStreet(district2),
+    "Shop1": createShop(district1),
+    "Shop2": createShop(district2),
+    "Location1": selectRandom(cess.General.Locations),
+    "Location2": selectRandom(cess.General.Locations)
   }
 
   return Intersectionobj;
 }
 
-function createNPC(dist){
+function createNPC(dist) {
 
   let NPCobj = {
-    Name: selectRandom(cess[dist].NPCNames),
-    Job: selectRandom(cess[dist].NPCJobs),
-    Quirk: selectRandom(cess[dist].NPCQuirk),
-    Appearance: null,
-    Mannerism: null,
-    Faction: null,
-    Disease: null,
-    Change: null,
-    Boon: null,
-    Items: [],
-    Spell: null
+    "Name": selectRandom(cess[dist].NPCNames),
+    "Job": selectRandom(cess[dist].NPCJobs),
+    "Quirk": selectRandom(cess[dist].NPCQuirks),
+    "Appearance": "",
+    "Mannerism": "",
+    "Faction": "",
+    "Mod": "",
+    "Items": [],
+    "Spell": ""
   }
 
   if (Math.random() < .15) { //15% chance to belong to a faction
-    NPCobj[Faction] = selectRandom(cess[dist].Factions);
+    NPCobj["Faction"] = selectRandom(cess[dist].Factions);
   }
 
   if (Math.random() < .25) { //25% chance to have appearance
-    NPCobj[Appearance] = selectRandom(cess.General.NPCAppearances);
+    NPCobj["Appearance"] = selectRandom(cess.General.NPCAppearances);
   }
 
   if (Math.random() < .25) { //25% chance to have a mannerism
-    NPCobj[Mannerism] = selectRandom(cess.General.NPCMannerisms);
+    NPCobj["Mannerism"] = selectRandom(cess.General.NPCMannerisms);
   }
 
-  if (Math.random() < .1) { //10% chance to have a disease
-    NPCobj[Disease] = selectRandom(cess.General.Diseases);
-  }
-
-  if (Math.random() < .1) { //10% chance to have a change
-    NPCobj[Change] = selectRandom(cess[dist].Changes);
-  }
-
-  if (Math.random() < .1) { //10% chance to have a boon
-    NPCobj[Boon] = selectRandom(cess[dist].Boons);
+  //Chance to have a boon, bane, or disease
+  randMod = Math.random();
+  if (randMod < .2) {
+    disease = selectRandom(cess.General.Diseases);
+    NPCobj["Mod"] = "They are infected with " + disease[0] + ": " + disease[1];
+  } else if (randMod < .4) {
+    NPCobj["Mod"] = "The city has changed them, granting " + selectRandom(cess[dist].Changes);
+  } else if (randMod < .6) {
+    NPCobj["Mod"] = "The city has changed them, granting " + selectRandom(cess[dist].Boons);
   }
 
   //chances up to 3 items
   if (Math.random() < .5) {
-    NPCobj[items].concat(selectRandom(cess[dist].Items))
+    NPCobj["Items"].push(selectRandom(cess[dist].Items))
   }
   if (Math.random() < .5) {
-    NPCobj[items].concat(selectRandom(cess[dist].Items))
+    NPCobj["Items"].push(selectRandom(cess[dist].Items))
   }
   if (Math.random() < .5) {
-    NPCobj[items].concat(selectRandom(cess[dist].Items))
+    NPCobj["Items"].push(selectRandom(cess[dist].Items))
+  }
+  if (dist != "Cobblestone") {
+    if (Math.random() < .5) {
+      NPCobj["Items"].push("(Contraband) " + selectRandom(cess[dist].Contraband))
+    }
   }
 
   //Chance to know a Spell
   if (Math.random() < .5) {
-    NPCobj[Spell] = (selectRandom(cess.General.Spells));
+    NPCobj["Spell"] = (selectRandom(cess.General.Spells));
   }
 
   return NPCobj;
 }
 
-function paveStreet(dist){
+function paveStreet(dist) {
 
   let StreetObj = {
-    Name: null,
-    Appearance: selectRandom(cess[dist].StreetAppearances),
+    "Name": null,
+    "Appearance": selectRandom(cess[dist].StreetAppearances)
   }
 
-  if (dist = "Cobblestone"){ //more variety
-    StreetObj[Name] = selectRandom(cess[dist].StreetPrefixes) + " " + selectRandom(cess[dist].StreetSuffixes);
-  } else { 
-    StreetObj[Name] = selectRandom(cess[dist].StreetNames)
+  if (dist == "Cobblestone") { //more variety
+    StreetObj["Name"] = selectRandom(cess[dist].StreetPrefixes) + " " + selectRandom(cess[dist].StreetSuffixes);
+  } else {
+    StreetObj["Name"] = selectRandom(cess[dist].StreetNames);
   }
 
   return StreetObj;
 }
 
-function createShop(dist){
+function createShop(dist) {
   let ShopObj = {
-    Name: selectRandom(cess[dist].ShopNames),
-    Quirk: selectRandom(cess[dist].NPCQuirk),
-    NPCs: [createNPC(dist)],
-    Inside: null,
-    Reputation: null,
-    Contraband: [],
-    Artifacts: [],
-    Items: []
-  }
-
-  if (Math.random() < .25) { //25% chance to have an Inside
-    ShopObj[Inside] = selectRandom(cess.General.ShopInsides);
-  }
-
-  if (Math.random() < .25) { //25% chance to have a Reputation
-    ShopObj[Reputation] = selectRandom(cess.General.ShopReputations);
-  }
-
-  //Chance for more NPCs
-  if (Math.random() < .5) {
-    ShopObj[NPCs].concat(createNPC(dist))
-  }
-  if (Math.random() < .5) {
-    ShopObj[NPCs].concat(createNPC(dist))
+    "Name": selectRandom(cess[dist].ShopNames),
+    "Quirk": selectRandom(cess[dist].ShopQuirks),
+    "Keep": createNPC(dist),
+    "Contraband": [],
+    "Artifacts": [],
+    "Reputation": "",
+    "Items": []
   }
 
   //Chance for contraband (no cobblestone contraband)
-  if (dist != "Cobblestone"){
-    if (Math.random() < .5) {
-      ShopObj[Contraband].concat(cess[dist].Contraband)
-    }
-    if (Math.random() < .5) {
-      ShopObj[Contraband].concat(cess[dist].Contraband)
+  if (dist != "Cobblestone") {
+    if (Math.random() < .75) {
+      ShopObj["Contraband"].push(cess[dist].Contraband)
     }
   }
 
   //Items sold
-  for (i=0;i<10;i++){
+  for (let i = 0; i < 10; i++) {
     if (Math.random() < .5) {
-      ShopObj[Items].concat(selectRandom(cess[dist].Items))
+      ShopObj["Items"].push(selectRandom(cess[dist].Items))
     }
   }
 
-  //Chance for artifacts
-  if (Math.random() > .5){
-    ShopObj[Artifacts].concat(selectRandom(cess[dist].Artifacts))
-  }
-  if (Math.random() > .5){
-    ShopObj[Artifacts].concat(selectRandom(cess[dist].Artifacts))
+  //Chance for artifact
+  if (Math.random() < .75) {
+    ShopObj["Artifacts"].push(selectRandom(cess[dist].Artifacts))
   }
 
-  return ShopObj;
+  //Chance for reputation
+  if (Math.random() > .5) {
+    ShopObj["Reputation"].concat(selectRandom(cess.General.ShopReputations))
+  }
 
+  //To String
+  ShopHTML = "<h3>" + ShopObj.Name + "</h3>" + "<p>" + ShopObj.Quirk;
+  if (ShopObj.Reputation != "") {
+    ShopHTML = ShopHTML + ShopObj.Reputation;
+  }
+  ShopHTML = ShopHTML + "</p>";
+
+  ShopHTML = ShopHTML + "<p><strong>Shopkeep</strong>: " + ShopObj.Keep.Name + ". " + ShopObj.Keep.Quirk + "</p>";
+
+  itemText = "<p><strong>Inventory</strong>:<ul>";
+  for (item in ShopObj.Items) {
+    itemText = itemText + "<li>" + ShopObj.Items[item] + "</li>";
+  }
+  for (cont in ShopObj.Contraband) {
+    itemText = itemText + "<li><strong>Contraband: </strong>" + ShopObj.Contraband[cont][0] + ". " + ShopObj.Contraband[cont][1] + "</li>";
+  }
+  for (arts in ShopObj.Artifacts) {
+    itemText = itemText + "<li><strong>Artifact: </strong>" + ShopObj.Artifacts[arts][0] + ". " + ShopObj.Artifacts[arts][1] + "</li>";
+  }
+  itemText = itemText + "</ul></p>";
+
+  ShopHTML = ShopHTML + itemText;
+
+  return ShopHTML;
 }
 
-function randEncounter(dist, dist){
+function randEncounter() {
   //pull from both district encounters AND general encounters
-  enc = selectRandom(cess[dist].Encounters.concat(cess[dist].Encounters, cess.General.Encounters))
-
-  //[0] = name, [1,2,3,4] = mood
-  let EncounterObj = {
-    Name: enc.shift(), //this removes the first element
-    Mood: selectRandom(enc) //then picks a random mood
-  }
-
-  return EncounterObj;
+  //first element is the name, second element randomly picks the mood.
+  enc = selectRandom(cess[district1].Encounters.concat(cess[district2].Encounters, cess.General.Encounters))
+  document.getElementById("randBtn").innerHTML = "<p><strong>" + enc.shift() + "</strong></p><p>Mood: " + selectRandom(enc) + "</p>";
 }
-
