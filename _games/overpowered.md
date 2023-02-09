bf5de2344179695b26210dc39feeb0bac6640692
David Schirduan <davidschirduan@gmail.com>
2023-02-09 17:19:51 +0000

    more bot generaation

--- _games/overpowered.md
+++ _games/overpowered.md
@@ -39,7 +39,7 @@ Over the next few months I hope to release more content for the **Deluxe Edition
 <div class="row">
   <div id="botDetails" class="col-xl col-12">
     <h3 id="botName" class="majorDisplay">Normal.Beast.Y</h3>
-    <div class="row">
+    <div class="row" style="padding-right: 2rem;">
       <div class="col-lg-4 col">
       <img id="osrImg" style="width: 100%;" src="/images/overpoweredExamples/OSR4.gif">
       </div>
@@ -47,7 +47,7 @@ Over the next few months I hope to release more content for the **Deluxe Edition
         <ul>
         <li id="osrWeapon"><strong>Stun Prod:</strong> short range electrical zaps. <span>(HEALTH / CON / MAGIC DEF)</span></li>
         <li id="osrDefense"><strong>Cloak:</strong> avoid visual detection. <span>(PERCEPTION / DEX / WIS)</span></li>
-        <li id="osrExplore"><strong>Drill:</strong> loudly tunnel through obstacles. <span>(DC / HEALTH / ARMOR)</span></li>
+        <li id="osrTool"><strong>Drill:</strong> loudly tunnel through obstacles. <span>(DC / HEALTH / ARMOR)</span></li>
         <li id="osrTalk"><strong>Translator:</strong> speak any language with robo voice. <span>(WIS / LANGUAGE / INT)</span></li>
         <li id="osrMove"><strong>Wheels:</strong> fast and stable. <span>Can't jump or climb.</span></li>
         </ul>
--- assets/generator_resources/overpowered.js
+++ assets/generator_resources/overpowered.js
@@ -350,12 +350,29 @@ function generateBotDetails(oldSeed){
     botName = oldSeed;
   }
   myrng = new Math.seedrandom(botName);
+  document.getElementById('botName').innerText = botName;
 
   weaponChoice = overpowered.Weapons[Math.floor(myrng() * overpowered.Weapons.length)];
-  weaponsHTML = "<strong>" + weaponChoice.Name + ":</strong> " + weaponChoice.Description + " <span>(" + 
+  document.getElementById('osrWeapon').innerHTML = "<strong>" + weaponChoice.Name + ":</strong> " + weaponChoice.Description + " <span>(" + 
   weaponChoice.Stats[0] + " / " + weaponChoice.Stats[1] + " / " + weaponChoice.Stats[2] + ")</span>";
 
-  document.getElementById('osrWeapon').innerHTML = weaponsHTML;
-  document.getElementById('botName').innerText = botName;
+  defChoice = overpowered.Defenses[Math.floor(myrng() * overpowered.Defenses.length)];
+  document.getElementById('osrDefense').innerHTML = "<strong>" + defChoice.Name + ":</strong> " + defChoice.Description + " <span>(" + 
+  defChoice.Stats[0] + " / " + defChoice.Stats[1] + " / " + defChoice.Stats[2] + ")</span>";
+
+  toolChoice = overpowered.Tools[Math.floor(myrng() * overpowered.Tools.length)];
+  document.getElementById('osrTool').innerHTML = "<strong>" + toolChoice.Name + ":</strong> " + toolChoice.Description + " <span>(" + 
+  toolChoice.Stats[0] + " / " + toolChoice.Stats[1] + " / " + toolChoice.Stats[2] + ")</span>";
+
+  talkChoice = overpowered.Communications[Math.floor(myrng() * overpowered.Communications.length)];
+  document.getElementById('osrTalk').innerHTML = "<strong>" + talkChoice.Name + ":</strong> " + talkChoice.Description + " <span>(" + 
+  talkChoice.Stats[0] + " / " + talkChoice.Stats[1] + " / " + talkChoice.Stats[2] + ")</span>";
+
+  moveChoice = overpowered.Movement[Math.floor(myrng() * overpowered.Movement.length)];
+  document.getElementById('osrMove').innerHTML = "<strong>" + moveChoice.Name + ":</strong> " + moveChoice.Description + "<br>" + 
+  moveChoice.Stats[0] + "<br>" + moveChoice.Stats[1] + "<br>" + moveChoice.Stats[2];
+
+  //replace this with the fancy bot generator eventually
+  document.getElementById('osrImg').src = "/images/overpoweredExamples/OSR" + (Math.floor(Math.random() * 7) + 1) + ".gif"
 
 }
