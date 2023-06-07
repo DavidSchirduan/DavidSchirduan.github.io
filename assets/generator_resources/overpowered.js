1820dcd3fa4c2c3238cfcd25566d87dc3c82520b
David Schirduan <davidschirduan@gmail.com>
2023-06-07 14:03:48 -0400

    Update overpowered.js

--- assets/generator_resources/overpowered.js
+++ assets/generator_resources/overpowered.js
@@ -131,8 +131,7 @@ function grabParamsURL() {
 //setup the pools and vars
 var overpowered = {};
 botName = "ERROR.7";
-var seedrandom = require('seedrandom');
-var myrng = seedrandom(botName);
+var myrng = Math.random();
 
 //dice are notated: 4-1 for a d4 showing 1. 20-13-s for a d20 showing 13 that is selected. 
 treasurePool = [];
