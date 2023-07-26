e982fd37fd9c59ba1e36914ef96034a5883986e1
David Schirduan <davidschirduan@gmail.com>
2023-07-26 09:00:35 -0400

    Update overpowered.js

--- assets/generator_resources/overpowered.js
+++ assets/generator_resources/overpowered.js
@@ -557,7 +557,7 @@ function rerollDice() {
 
   if (enableEffects) {
     var duration = 1000;
-    const dice = document.querySelectorAll(".dicierHeavy:not(.dwhite):not(dRoller)");
+    const dice = document.querySelectorAll(".dicierHeavy:not(.dwhite, .dRoller)");
     let startTimestamp = null;
     var lastProgress = 0;
     const step = (timestamp) => {
